60b48d7a1d45d4153543d55b8fd47dc516dc2111
Qing Li <12790403+QingLea@users.noreply.github.com>
2024-05-22 17:13:21 +0300

    add user service

--- MetaScrapeHub/settings.py
+++ MetaScrapeHub/settings.py
@@ -35,8 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'scraping',
-    "rest_framework",
+    'user_service',
 ]
 
 MIDDLEWARE = [
