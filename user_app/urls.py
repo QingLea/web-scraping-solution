79401cbf8edd2b50eca48e0c673b98e0f1908b44
Qing Li <12790403+QingLea@users.noreply.github.com>
2024-05-23 16:24:33 +0300

    user profile

--- user_app/urls.py
+++ user_app/urls.py
@@ -4,7 +4,7 @@ from .views import LoginView, SignupView, UpdateAccountView, UserProfileView
 
 urlpatterns = [
     path("login/", LoginView.as_view()),
-    path("user/", UserProfileView.as_view()),
+    path("profile/", UserProfileView.as_view()),
     path("signup/", SignupView.as_view()),
     path("account/", UpdateAccountView.as_view()),
 ]
