c22427e321c57d231057320775800eee31d90941
Qing Li <12790403+QingLea@users.noreply.github.com>
2024-05-26 05:00:05 +0300

    fix serializers

--- meta_scrape_hub/settings.py
+++ meta_scrape_hub/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'meta_scrape_hub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'meta_scrapping_solution',
+        'USER': 'liqing',
+        'PASSWORD': 'liqing',
+        'HOST': 'localhost',  # Use 'localhost' for local development
+        'PORT': '5432',  # Default port for PostgreSQL
     }
 }
 
--- search/serializers.py
+++ search/serializers.py
@@ -4,9 +4,21 @@ from common.models import Product, Category
 
 
 class ProductReadSerializer(serializers.ModelSerializer):
+    category = serializers.SerializerMethodField()
+    sub_category = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
         fields = "__all__"
+        # Add `category_name` to the fields
+        fields = ["id", "name", "category", "sub_category", "price", "comparison_price", "comparison_unit", "currency",
+                  "image", "store", "created", "updated"]
+
+    def get_category(self, obj):
+        return obj.category.name if obj.category else None
+
+    def get_sub_category(self, obj):
+        return obj.sub_category.name if obj.sub_category else None
 
 
 class CategoryReadSerializer(serializers.ModelSerializer):
